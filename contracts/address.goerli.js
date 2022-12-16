72ced2c18648e5205ac8284f9e1ce1a312d7628e
charles-vj <charles2019@iiitkottayam.ac.in>
2022-12-17 03:27:08 +0530

    Feat: Finalized contract and deployed address

---
+++ contracts/address.goerli.js
@@ -0,0 +1,3 @@
+module.exports = {
+  STX: '0x37E90ceC5b6404604C30afc395c9C356b1522C7A',
+}
--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,14 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle')
 
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-ethers')
+require('dotenv').config()
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const accounts = [process.env.PRIVATE_KEY]
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -17,5 +15,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.9',
+  networks: {
+    mumbai: {
+      url: 'https://matic-mumbai.chainstacklabs.com',
+      chainId: 80001,
+      accounts,
+    },
+    goerli: {
+      url: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161	',
+      chainId: 5,
+      accounts,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN,
+  },
+}
